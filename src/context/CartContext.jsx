ada6887a8737171148b63e35879c0de5175e113f
Francis Nguyen <nguyenfrancis6@gmail.com>
2024-04-05 14:24:29 -0700

    Save cart data on refresh

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,54 +1,74 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const storedCart = localStorage.getItem("cart");
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
   const [totalValue, setTotalValue] = useState(0);
   const [totalQuantity, setTotalQuantity] = useState(0);
 
+  // Save cart data to local storage whenever cart state changes
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+  
   // find values of current cart
   useEffect(() => {
     let quantity = 0;
     let value = 0;
-    cart.forEach(item => {
+    cart.forEach((item) => {
       quantity += item.quantity;
       value += item.quantity * item.price;
-    })
-    setTotalQuantity(quantity)
-    setTotalValue(value)
-  }, [cart])
+    });
+    setTotalQuantity(quantity);
+    setTotalValue(value);
+  }, [cart]);
 
   const addProduct = (productId, price, title, image) => {
-    const existingProductIndex = cart.findIndex(item => item.id === productId);
+    const existingProductIndex = cart.findIndex(
+      (item) => item.id === productId
+    );
     if (existingProductIndex !== -1) {
       const updatedCart = [...cart];
       updatedCart[existingProductIndex].quantity += 1;
       setCart(updatedCart);
     } else {
-      setCart([...cart, { id: productId, quantity: 1, price: price, title: title, image: image }]);
+      setCart([
+        ...cart,
+        {
+          id: productId,
+          quantity: 1,
+          price: price,
+          title: title,
+          image: image,
+        },
+      ]);
     }
   };
 
-
   const removeProduct = (productId) => {
-    const existingProductIndex = cart.findIndex(item => item.id === productId);
+    const existingProductIndex = cart.findIndex(
+      (item) => item.id === productId
+    );
     if (existingProductIndex === -1) {
-      console.error("Not in your cart!")
-    }
-    else if (cart[existingProductIndex].quantity > 1) {
-      const updatedCart = [...cart]
+      console.error("Not in your cart!");
+    } else if (cart[existingProductIndex].quantity > 1) {
+      const updatedCart = [...cart];
       updatedCart[existingProductIndex].quantity -= 1;
-      setCart(updatedCart)
-    }
-    else {
-    const updatedCart = cart.filter(item => item.id !== productId);
-    setCart(updatedCart);
+      setCart(updatedCart);
+    } else {
+      const updatedCart = cart.filter((item) => item.id !== productId);
+      setCart(updatedCart);
     }
   };
 
   return (
-    <CartContext.Provider value={{ cart, addProduct, removeProduct, totalQuantity, totalValue }}>
+    <CartContext.Provider
+      value={{ cart, addProduct, removeProduct, totalQuantity, totalValue }}
+    >
       {children}
     </CartContext.Provider>
   );
